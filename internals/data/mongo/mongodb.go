8bc170ef78ccb23c8a11ef440839f84071aca618
Ahmad Addel <a4amado@gmail.com>
2025-12-24 16:53:43 +0200

    fix mongodbClient typo

--- internals/data/mongo/mongodb.go
+++ internals/data/mongo/mongodb.go
@@ -9,22 +9,22 @@ import (
 	"go.mongodb.org/mongo-driver/v2/mongo/options"
 )
 
-var monogdbClinet *mongo.Client
-var monogdbClinetOnce = sync.Once{}
+var mongodbClient *mongo.Client
+var mongodbClientOnce = sync.Once{}
 
 // Client returns the package-level singleton MongoDB client, initializing it on first call.
 // It connects using the MONGO_HISTORY_DATABASE_URL environment variable and panics if the initial connection fails.
 // Each invocation also accesses the database and collection named by MONGO_HISTORY_DATABASE_NAME and MONGO_HISTORY_DATABASE_APP_HISTORY_COLLECTION_NAME.
 func Client() *mongo.Client {
-	monogdbClinetOnce.Do(func() {
+	mongodbClientOnce.Do(func() {
 		client, err := mongo.Connect(options.Client().ApplyURI(os.Getenv(string(envConsts.MONGO_HISTORY_DATABASE_URL))))
 		if err != nil {
 			panic(err)
 		}
-		monogdbClinet = client
+		mongodbClient = client
 	})
 
-	monogdbClinet.Database(os.Getenv(string(envConsts.MONGO_HISTORY_DATABASE_NAME))).Collection(string(envConsts.MONGO_HISTORY_DATABASE_APP_HISTORY_COLLECTION_NAME))
-	return monogdbClinet
+	mongodbClient.Database(os.Getenv(string(envConsts.MONGO_HISTORY_DATABASE_NAME))).Collection(string(envConsts.MONGO_HISTORY_DATABASE_APP_HISTORY_COLLECTION_NAME))
+	return mongodbClient
 
 }
