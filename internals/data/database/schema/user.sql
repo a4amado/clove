b28bbea0ebc903633f5ba47a8d462f35355b89ff
Ahmad Addel <a4amado@gmail.com>
2025-12-24 16:51:52 +0200

    removed "userHistory" table, history will be handled using a time series db

--- internals/data/database/schema/user.sql
+++ internals/data/database/schema/user.sql
@@ -5,22 +5,4 @@ CREATE TABLE "user" (
     "createdAt" TIMESTAMP DEFAULT NOW(),
     "updatedAt" TIMESTAMP DEFAULT NOW()
 );
-
-CREATE TYPE changeOperation AS ENUM ('INSERT', 'DELETE', 'UPDATE');
-
-CREATE TABLE "userHistory" (
-    "id" UUID PRIMARY KEY DEFAULT(uuid_generate_v4()),
-    "userId" UUID NOT NULL,
-    "email" VARCHAR(255) NOT NULL,  -- STILL WRONG - REMOVE UNIQUE
-    "hash" TEXT NOT NULL,  -- YOU'RE STILL MISSING THIS
-    "createdAt" TIMESTAMP NOT NULL,
-    "updatedAt" TIMESTAMP NOT NULL,
-    "changedAt" TIMESTAMP NOT NULL DEFAULT NOW(),
-    "changedBy" UUID,  -- SHOULD BE NULLABLE
-    "operation" changeOperation NOT NULL,
-    
-    CONSTRAINT fk_user_id
-        FOREIGN KEY ("userId")
-        REFERENCES "user"("id")
-        ON DELETE CASCADE
-);
+ 
